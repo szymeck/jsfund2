fdc4086ce443327ae0193169fa40824076ad2c20
Szymon <szymek.sobierajski@gmail.com>
2021-11-15 22:06:05 +0100

    nowe_zmiany

--- starter/assigment.js
+++ starter/assigment.js
@@ -6,3 +6,20 @@ function describeCountry(country, population, capitalCity) {
 }
 const countryCheck = describeCountry('germany', 60, 'berlin');
 console.log(countryCheck);
+
+function percentageOfWorld1(population) {
+    const percent = population / 7900 * 100;
+    return percent;
+}
+
+const percentageOfWorld2 = function (population) {
+    const percent = population / 7900 * 100;
+    return percent;
+
+}
+
+const percentageOfWorld3 = population => population / 7900 * 100;
+const percPol = percentageOfWorld3(39);
+const percGer = percentageOfWorld3(60);
+const percUkr = percentageOfWorld3(55);
+console.log(percPol, percGer, percUkr);
--- starter/script.js
+++ starter/script.js
@@ -24,3 +24,31 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+function calcAge1(birthYear) {
+    return 2021 - birthYear;
+}
+
+const age1 = calcAge1(1987);
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+    return 2021 - birthYear;
+}
+const age2 = calcAge2(1987);
+console.log(age1, age2);
+
+const calcage3 = birthYear => 2021 - birthYear;
+const age3 = calcage3(1987);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+    const age = 2021 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearUntilRetirement(1987, "szymi"));
+console.log(yearUntilRetirement(1983, "mati"));
+
